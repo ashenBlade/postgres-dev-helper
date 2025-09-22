c01017f4d8fd3b0e0a01e3da1c10a6013be64acf
Sergey Solovev <ashen.blade@ya.ru>
2025-09-22 22:34:54 +0300

    chore: add ESLint
    Added ESLint support with eslint.config.mjs configuration script.
    This is just copy from VS Code github example extensions, i.e.
    https://github.com/microsoft/vscode-extension-samples/blob/main/helloworld-sample/eslint.config.mjs
    
    But there are some custom settings added after.
    
    Also, using strict rules to find more possible errors, because I am not
    ts developer and do not know all the possible problems.

---
+++ eslint.config.mjs
@@ -0,0 +1,61 @@
+/**
+ * ESLint configuration for the project.
+ * 
+ * See https://eslint.style and https://typescript-eslint.io for additional linting options.
+ */
+// @ts-check
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
+import { defineConfig } from "eslint/config";
+
+export default defineConfig(
+    /* Copied from VS Code extension samples */
+    {
+        ignores: [
+            '.vscode-test',
+            'out',
+        ]
+    },
+    js.configs.recommended,
+    ...tseslint.configs.strict,
+    ...tseslint.configs.stylistic,
+    {
+        plugins: {
+            '@stylistic': stylistic
+        },
+        rules: {
+            'curly': 'warn',
+            '@stylistic/semi': ['warn', 'always'],
+            '@typescript-eslint/no-empty-function': 'off',
+            '@typescript-eslint/naming-convention': [
+                'warn',
+                {
+                    'selector': 'import',
+                    'format': ['camelCase', 'PascalCase']
+                }
+            ],
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    'argsIgnorePattern': '^_'
+                }
+            ],
+        },
+    },
+    
+    /* Custom settings */
+    {
+        plugins: {
+            '@stylistic': stylistic,
+        },
+        rules: {
+            /* Declare variables in switch/case */
+            'no-case-declarations': 'off',
+            /* Features class */
+            '@typescript-eslint/no-extraneous-class': [ "error", { "allowStaticOnly": true }],
+            /* TestMode enum in tests */
+            '@typescript-eslint/prefer-literal-enum-member': ['warn', {'allowBitwiseExpressions': true}],
+        }
+    }
+);
