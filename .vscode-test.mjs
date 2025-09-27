df58d566cd582393007a8fbdf2f765e82291c98f
Sergey Solovev <ashen.blade@ya.ru>
2025-09-27 17:13:05 +0300

    test: vscode test integration
    Previously all tests were run using test.sh script, but it does not
    allow to debug source code. As unit tests were added it became necessary
    to be able to debug source code.
    
    File .vscode-test.mjs is detected by vscode and then shown in test
    explorer, so I can run any test any time.

---
+++ .vscode-test.mjs
@@ -0,0 +1,23 @@
+// .vscode-test.js
+import { defineConfig } from '@vscode/test-cli';
+
+// npx vscode-test --config .vscode-test.mjs
+export default defineConfig([
+    {
+        label: 'Unit tests',
+        files: 'out/test/**/*.test.js',
+        mocha: {
+            ui: 'tdd',
+            timeout: 20000,
+            parallel: false,
+        },
+        version: 'stable',
+        workspaceFolder: './pgsrc/18',
+        env: {
+            PGHH_DEBUGGER: 'cppdbg',
+            PGHH_PG_VERSION: '18',
+            PGHH_VSCODE_VERSION: 'stable',
+            PGHH_TEST_MODE: 'vars,format,unit',
+        },
+    },
+]);
